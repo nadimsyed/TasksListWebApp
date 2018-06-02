abaf2364aeb06502fd8ec4ff78c1755bb7b300b4
Nadim <syed.m.nadim@gmail.com>
2018-06-02 17:42:46 -0400

    First commit

---
+++ TasksListWebApp/TasksListWebApp/Controllers/TasksController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TasksListWebApp.Models;
+
+namespace TasksListWebApp.Controllers
+{
+    public class TasksController : Controller
+    {
+        private TaskListEntities db = new TaskListEntities();
+
+        // GET: Tasks
+        public ActionResult Index()
+        {
+            ViewBag.Users = db.Users;
+
+            //var tasks = db.Tasks.Include(t => t.User);
+            //ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName");
+
+            return View(/*tasks.ToList()*/);
+        }
+
+        // GET: Tasks/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Task task = db.Tasks.Find(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            return View(task);
+        }
+
+        public ActionResult Listing(int UserId)
+        {
+            ////var x = db.Users.Include(t => t.UserName == userName);
+            ////int id =
+            //int id = int.Parse(input);
+
+            ViewBag.Tasks = db.Tasks.Where(t => t.UserId == UserId);
+            
+            return View();
+        }
+
+        // GET: Tasks/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName");
+            return View();
+        }
+
+        // POST: Tasks/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Description,DueDate,Completion,UserId")] Task task)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tasks.Add(task);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName", task.UserId);
+            return View(task);
+        }
+
+        // GET: Tasks/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Task task = db.Tasks.Find(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName", task.UserId);
+            return View(task);
+        }
+
+        // POST: Tasks/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Description,DueDate,Completion,UserId")] Task task)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(task).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName", task.UserId);
+            return View(task);
+        }
+
+        // GET: Tasks/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Task task = db.Tasks.Find(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            return View(task);
+        }
+
+        // POST: Tasks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Task task = db.Tasks.Find(id);
+            db.Tasks.Remove(task);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TasksListWebApp/TasksListWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TasksListWebApp.MvcApplication" Language="C#" %>
